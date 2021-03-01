5559ff89579f9277560055c694abed28b2433582
sarvang008 <sarvang.m@somaiya.edu>
2021-03-01 22:50:26 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
+interface countState{
+count:number
+
+}
 function App() {
+const [count,setCount]=useState<number>(0)
+
+const add=(e:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
+  setCount(prev=>prev+1)
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>{count}</h1>
+      <button onClick={e=>setCount(prev=>prev-1)}> -</button>
+
+      <button onClick={add}>+</button>
       </header>
     </div>
   );
